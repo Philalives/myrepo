24533479d1a6486adb6abf967ef5f6d48527f32b
Philani Mpofu <phmpofu@gmail.com>
2021-05-10 15:30:22 -0400

    added title to plot

--- rcode.R
+++ rcode.R
@@ -3,7 +3,7 @@ set.seed(2985)
 
 x <- rnorm(100)
 y <- 2*x + rnorm(100, mean=0, sd= 0.02)
-plot(x, y)
+plot(x, y, main="y vs x")
 # can make this better
 plot(x, y, type="o")
 # is this better for real?
